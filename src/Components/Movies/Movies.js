e2b69a616d8be6fac4cbcf7144974401e76c727d
ChukhinAI <55681756+ChukhinAI@users.noreply.github.com>
2021-02-17 17:08:00 +0300

    Update Movies.js
    hotfix для undefined title

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -10,10 +10,12 @@ export default class Movies extends React.Component{
     constructor(props) {
         super(props);
         this.updateMovies();
+        //this.updateMovies(); // net
     }
 
     onMovieLoaded = (movie) => {
         this.setState(()=>{
+
             this.state.movies.push(movie);
         } );
     };
@@ -21,14 +23,19 @@ export default class Movies extends React.Component{
     updateMovies(){
         for (let i = 0; i < 15; ++i){
             const id = Math.floor(Math.random() * (550 - 100) + 100);
-            this.api.getMovie(id).then(this.onMovieLoaded);
+
+                this.api.getMovie(id).then(this.onMovieLoaded);
+
         }
     }
 
     render(){
-        console.log("in render");
+        console.log("in render 1");
+        console.log("0 is ");
+
         console.log(this.state.movies);
         const elements = this.state.movies.map(({title}) => {
+            if (title != undefined) // hotfix пустых элементов с undefined title
             return (
                 <li className="list-group-item">
                     {title}
@@ -36,9 +43,22 @@ export default class Movies extends React.Component{
             );
         })
 
+        console.log("in render 2");
+        console.log(elements); // пустые на первом проходе
+
+        let elementsCopy;
+        /*
+        for (let tt of this.state.movies.map.values())
+        {
+            if (tt != undefined) // hotfix пустых элементов с undefined title
+                elementsCopy = this.state.movies.map.get(tt);
+        }
+*/
         return(
+
             <div className="movies">
                 <h2>Список фильмов</h2>
+
                 <ul className="list-group d-flex-row">
                     {elements}
                 </ul>
