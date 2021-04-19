7f788aadc20aabe7ab8c1ef779c764bf45e832c0
tefadivan <43060880+tefadivan@users.noreply.github.com>
2021-04-19 22:50:25 +0300

    Update tmdb-api.js

--- src/Services/tmdb-api.js
+++ src/Services/tmdb-api.js
@@ -3,17 +3,6 @@ export default class tmdbApi{
         this.baseUrl = 'https://api.themoviedb.org/3/movie/';
         this.apiKey = '?api_key=3e02319f0f5cca64a9821ef1961a6e97';
         this.basePosterUrl = 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2';
-        // ok :
-        // https://api.themoviedb.org/3/movie/100?api_key=3e02319f0f5cca64a9821ef1961a6e97
-        // id = 100
-
-        // 7 - no valid key
-        // https://api.themoviedb.org/3/movie/discover/movie?with_genres=18&primary_release_year=2014?api_key=3e02319f0f5cca64a9821ef1961a6e97 - no 7
-        // https://api.themoviedb.org/3/discover/movie?with_genres=18&primary_release_year=2014?api_key=3e02319f0f5cca64a9821ef1961a6e97 - no 7
-        // https://api.themoviedb.org/discover/movie?with_genres=18&primary_release_year=2014?api_key=3e02319f0f5cca64a9821ef1961a6e97 - no - 404
-
-        // images - http://image.tmdb.org/t/p/
-        // https://www.themoviedb.org/t/p/w600_and_h900_bestv2/57vIJPRNsBZUAKu0jkt8QrA3tXI.jpg - full, ok
         this.maxID = 100;
     }
 
@@ -26,6 +15,7 @@ export default class tmdbApi{
         const movie = await this.getResource(this.baseUrl + id + this.apiKey);
         return this._transformMovie(movie);
     }
+    
     _transformMovie(movie) {
         return {
             title: movie.title,
@@ -35,8 +25,6 @@ export default class tmdbApi{
             id: this.maxID++
         };
     }
-
-    ///discover/movie?sort_by=popularity.desc
 
     async getTopPopularMovies(){
         const movie = await this.getResource(
